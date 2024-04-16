2431644225ef46e61fc3c6842c596bdb7b2d3119
nyawox <nyawox.git@gmail.com>
2024-04-16 19:41:58 +0900

    update readme

--- flake.nix
+++ flake.nix
@@ -12,11 +12,13 @@
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
-      flake.nixosModules.nixtendo-switch = {
-        imports = [
-          ./switch-boot.nix
-          ./switch-presence.nix
-        ];
+      flake.nixosModules = {
+        nixtendo-switch = {
+          imports = [
+            ./switch-boot.nix
+            ./switch-presence.nix
+          ];
+        };
       };
     };
 }
