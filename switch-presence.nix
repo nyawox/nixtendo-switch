cff407a7c190a7ccfab4940792c2fc8e9fbbdf1c
nyawox <nyawox.git@gmail.com>
2024-04-16 20:48:03 +0900

    revert to mkDerivation

--- switch-presence.nix
+++ switch-presence.nix
@@ -6,7 +6,7 @@
 }:
 with lib; let
   cfg = config.services.switch-presence;
-  presence-client = pkgs.python3Packages.buildPythonApplication {
+  presence-client = pkgs.mkDerivation {
     pname = "presence-client";
     version = "unstable-2023-12-19";
     src = pkgs.fetchFromGitHub {
@@ -15,7 +15,6 @@ with lib; let
       rev = "890c510f27add83eaa0217444d7385cceb45ee08";
       hash = "sha256-34rCyEJ9jiAYxrOMo5uzuX1mi7Kw5vcZZc4p0NuIkeE=";
     };
-    doCheck = false;
     # Runtime dependencies
     propagatedBuildInputs = [
       (pkgs.python3.withPackages (pythonPackages:
@@ -25,7 +24,10 @@ with lib; let
           python-dotenv
         ]))
     ];
-    installPhase = "install -Dm755 PresenceClient/PresenceClent-Py/presence-client.py $out/bin/presence-client";
+    installPhase = ''
+      install -Dm755 PresenceClient/PresenceClent-Py/presence-client.py $out/bin/presence-client
+      sed -i '1 i #!/usr/bin/env python3' $out/bin/presence-client
+    '';
   };
 in {
   options = {
